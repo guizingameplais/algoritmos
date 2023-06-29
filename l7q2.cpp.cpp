92568499bc1038c044aaa8355e57b16eb0cacf98
guizingameplais <137493562+guizingameplais@users.noreply.github.com>
2023-06-29 13:25:49 -0300

    Add files via upload

---
+++ l7q2.cpp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+int main() {
+char x[]="eajufrn";
+  int i=0;
+  while(i < 4){
+    cout << x[i];
+    i++;
+  }
+    return 0;
+  
+   }
+
