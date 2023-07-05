6e36e18cc6cdf0861c952089baff2a664031eb87
guizingameplais <137493562+guizingameplais@users.noreply.github.com>
2023-07-04 22:16:42 -0300

    L4Q1.cpp

--- L4Q1.cpp
+++ L4Q1.cpp
@@ -7,7 +7,7 @@ int main() {
   //1. Faça um programa que leia uma string e a imprima na tela.
   //Guilherme Pinheiro,Turma IF23 
   string x;
-  cout << "digite o valor da string: " << endl;
+  cout << "digite o valor da string:" << endl;
   getline(cin,x);
   cout << "valor da string: " << x << endl;
 }
